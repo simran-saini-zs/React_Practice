d6c56c4a450f268992482ba62337147c74b5dcc1
Raramuri <raramuri@Raramuris-MacBook-Pro.local>
2025-01-15 11:51:45 +0530

    Assignment given by Anirudh on react hooks

--- src/App.jsx
+++ src/App.jsx
@@ -7,11 +7,14 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import ReactDOM from "react-dom"
+import {ColorProvider} from "./ColorContext.jsx"
 
 function App() {
   return (
     <>
-     <Assignment/>
+    <ColorProvider>
+    <Assignment/>
+    </ColorProvider>
     </>
   )
 }
