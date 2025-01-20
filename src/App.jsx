f0a8c7777fef16e1fafc2de5fe5c8d27f2ed13aa
Raramuri <raramuri@Raramuris-MacBook-Pro.local>
2025-01-20 17:43:46 +0530

    completed the project on react

--- src/App.jsx
+++ src/App.jsx
@@ -8,12 +8,15 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import ReactDOM from "react-dom"
 import {ColorProvider} from "./ColorContext.jsx"
+import Test from "./react-test.jsx"
 
 function App() {
+  let desc = "As a college student, much of your time will be spent interacting with texts of all types, shapes, sizes, and delivery methods. Sound interesting? Oh, it is. In the following sections, we’ll explore the nature of texts, what they will mean to you, and how to explore and use them effectively";
+  let title = "Title";
   return (
     <>
     <ColorProvider>
-    <Assignment/>
+    <Test source = {reactLogo} title = {title} desc = {desc}/>
     </ColorProvider>
     </>
   )
