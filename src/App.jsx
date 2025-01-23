e1f18bb74ddc472b33cf4a5f9a216c9ff6d85680
Raramuri <raramuri@Raramuris-MacBook-Pro.local>
2025-01-23 13:03:32 +0530

    Completed assignment on modal

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,20 @@
-import { createContext, useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-import Assignment from "./Assignment.jsx"
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import ReactDOM from "react-dom"
-import {ColorProvider} from "./ColorContext.jsx"
-import Counter from './Counter.jsx'
-import Form from './Form.jsx'
-import Problem1 from './Problem1SetTimeOut.jsx'
-import WindowHook from './WindowHook.jsx'
-import MountedHook from './MountedHook.jsx'
-import RefHook from './RefHook.jsx'
+// import './App.css'
+// import './index.css'
+import './modal.css'
+import Modal from './modal.jsx'
+import { useState } from 'react'
 function App() {
-  let {w_size,h_size} = WindowHook();
-  let {ismount} = MountedHook();
-  console.log(ismount);
-  let {ref,size} = RefHook();
+  let [show,setShow] = useState(false);
+  function handleclick()
+  {
+    setShow((prev)=>!prev);
+  }
   return (
     <>
-    <ColorProvider>
-    <div>
-        <h1>Screen Size = {w_size*h_size}</h1>
-        <h2>Height = {h_size}</h2> 
-        <h2>Width = {w_size}</h2>
-        {ismount && <h3>Hi</h3>}
-        <div ref ={ref}>This is to show RefHook {size}</div>
+    <div className= "mainDiv">
+    <button onClick = {handleclick} className='btn'>Show Content</button>
+    {show && <Modal handleclick = {handleclick} className = 'modal'/>}
     </div>
-    </ColorProvider>
     </>
   )
 }
